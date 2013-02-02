eee1e49f5edb0f1d1bc3eeeb1ccd2ea62a07bfb0
Yoshinori Tahara <read.eval.print@gmail.com>
2013-02-02 23:38:08 +0900

    今回は fiveam 使ってみる。

--- test.lisp
+++ test.lisp
@@ -1,10 +1,66 @@
-(ql:quickload :info.read-eval-print.mongo)
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload :info.read-eval-print.mongo)
+  (ql:quickload :fiveam))
 
 (defpackage :info.read-eval-print.mongo.test
-  (:use :cl :info.read-eval-print.mongo :info.read-eval-print.bson)
-  (:shadowing-import-from :info.read-eval-print.mongo #:delete #:find))
+  (:use :cl :info.read-eval-print.mongo :info.read-eval-print.bson :fiveam)
+  (:shadowing-import-from :info.read-eval-print.mongo #:delete #:find)
+  (:import-from :fiveam #:def-suite*))
 
 (in-package :info.read-eval-print.mongo.test)
+
+(defparameter *test-mongod* "/usr/bin/mongod")
+(defparameter *test-db-dir* "/tmp/info.read-eval-print.mongo.test")
+(defparameter *test-port* 27077)
+
+(defun run-test-mongod ()
+  (asdf:run-shell-command "rm -r ~a" *test-db-dir*)
+  (ensure-directories-exist (concatenate 'string *test-db-dir* "/"))
+  (let ((asdf::*verbose-out* *terminal-io*))
+    (asdf:run-shell-command
+     "~a --dbpath ~a --port ~a --pidfilepath ~a/mongod.pid ~
+      --fork --logpath ~a/mongod.log --nojournal --noprealloc --smallfiles"
+     *test-mongod* *test-db-dir* *test-port* *test-db-dir* *test-db-dir*)))
+
+(defun kill-test-mongod ()
+  (asdf:run-shell-command "kill `cat ~a/mongod.pid`" *test-db-dir*))
+
+(defmacro with-test-mongod (&body body)
+  `(unwind-protect
+        (progn
+          (run-test-mongod)
+          ,@body)
+     (kill-test-mongod)))
+
+
+(defmacro with-test-collection ((collection &optional (collection-name "cons")) &body body)
+  (let ((connection (gensym "connection"))
+        (db (gensym "db")))
+    `(with-test-mongod
+       (with-connection (,connection :port *test-port*)
+         (let* ((,db (db ,connection "test"))
+                (,collection (collection ,db ,collection-name)))
+           ,@body)))))
+
+(def-suite all)
+
+(def-suite* mongo :in all)
+
+(test test-basic-operations
+  (with-test-collection (collection)
+    (insert collection (bson "foo" "bar"))
+    (let ((docs (loop with cursor = (find collection)
+                      while (next-p cursor)
+                      collect (next cursor))))
+      (is (= 1 (length docs)))
+      (is (string= (value (car docs) "foo") "bar")))
+    (delete collection (bson "foo" "bar"))
+    (let ((docs (loop with cursor = (find collection)
+                      while (next-p cursor)
+                      collect (next cursor))))
+      (is (null docs)))))
+
+(debug!)
 
 #+nil
 (with-connection (connection)
@@ -35,6 +91,7 @@
          (collection (collection db "test")))
     (delete collection)))
 
+#+nil
 (with-connection (connection)
   (let* ((db (db connection "test"))
          (collection (collection db "test")))
