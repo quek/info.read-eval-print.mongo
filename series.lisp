29bcaa10acd6bfe8264eaccd489613a54bdcb299
Yoshinori Tahara <read.eval.print@gmail.com>
2013-04-11 21:54:35 +0900

    scan-mongo に :projection 追加した。 :fields の方がいい?

--- series.lisp
+++ series.lisp
@@ -1,10 +1,10 @@
 (in-package #:info.read-eval-print.mongo)
 
-(series::defS scan-mongo (collection query &key (skip 0) (limit 0) sort)
+(series::defS scan-mongo (collection query &key (skip 0) (limit 0) sort projection)
   "scan mongoDB collection."
   (series::fragl
    ;; args
-   ((collection) (query) (skip) (limit) (sort))
+   ((collection) (query) (skip) (limit) (sort) (projection))
    ;; rets
    ((bson t))
    ;; aux
@@ -12,7 +12,11 @@
    ;; alt
    ()
    ;; prolog
-   ((setq cursor (find collection query :skip skip :limit limit :sort sort)))
+   ((setq cursor (find collection query
+                       :skip skip
+                       :limit limit
+                       :sort sort
+                       :projection projection)))
    ;; body
    ((unless (next-p cursor)
       (go series::end))
